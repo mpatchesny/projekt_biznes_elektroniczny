549e9903f46de2a5eae0de2d8fb3649eba7b9b2f
MP <paczesnymaciej@gmail.com>
2022-10-19 23:12:27 +0200

    add basic functionality

--- scripts/product_import/generate_insert_product_query.py
+++ scripts/product_import/generate_insert_product_query.py
@@ -13,10 +13,21 @@ def replacePolishDiacritics(text):
         text = text.replace(polish[i], latin[i])
     return text
 
-def main(argv):
-    with open(argv[0], "r") as f:
-        scrapData = json.loads(f.read())
-        # TODO
+def generateQueryForProduct(product, queryTemplate) -> str:
+    pass
 
+def generateQuery(scrapData, queryTemplate) -> list:
+    pass
+
+def main(argv):
+    with open(argv[0], "r", encoding="utf-8") as f:
+        scrapData = json.loads(f.read())
+        
+        with open(argv[1], "r", encoding="utf-8") as f2:
+            queryTemplate = f2.read()
+            sql = generateQuery(scrapData, queryTemplate)
+
+            with open("insert_product.sql", "w", encoding="utf-8") as f3:
+                f3.write(sql.join("\n\n\n"))
 
 main(sys.argv[1:])
