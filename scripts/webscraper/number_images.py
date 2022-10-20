c978eda537e560ce9e3252bc73a7b9c52905e44d
MP <paczesnymaciej@gmail.com>
2022-10-20 23:24:20 +0200

    yet another small fix

--- scripts/webscraper/number_images.py
+++ scripts/webscraper/number_images.py
@@ -9,37 +9,30 @@ import json
 import math
 import shutil
 
-# Sczytywanie obrazków z folderu images w kolejności
-
-images=[]
-for _, _, files in os.walk("images"):
-    for x in files:
-        path = os.getcwd() + "\\images\\" + x
-        images.append(path)
+# Zapisywanie obrazków z folderu images w kolejności
 
 with open("result_no_dumps.json", "r") as f:
     results = json.loads(f.read())
 
-startImageId = 1
+imageId = 1
 filesPerFolder = 20
 for x in results:
     if x.get("local_path"):
         filename = x["local_path"]
         filename = filename.split("\\")[-1]
-        localFixed = os.getcwd() + "\\images\\" + filename
-        if localFixed in images:
-            position = images.index(localFixed)
-            imageId = startImageId + position
-
+        localPathFixed = os.getcwd() + "\\images\\" + filename
+        
+        if os.path.exists(localPathFixed):
             newImagePath = os.getcwd() + "\\images\\" + str(math.ceil(imageId/ filesPerFolder))
             if not os.path.exists(newImagePath):
                 os.mkdir(newImagePath)
             newImageName = str(imageId) + ".jpg"
             newImagePath = newImagePath + "\\" + newImageName
 
-            shutil.copy(localFixed, newImagePath)
+            shutil.copy(localPathFixed, newImagePath)
             x["image_id"] = imageId
             x["local_path_fixed"] = newImagePath
+            imageId += 1
         else:
             print(localFixed + " nie znaleziono")
     else:
