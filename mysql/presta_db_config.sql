7f39271484e5a725ff32351f60f69562ecdb114b
MP <paczesnymaciej@gmail.com>
2023-01-03 23:44:32 +0100

    remove

--- mysql/presta_db_config.sql
+++
@@ -1,5 +0,0 @@
-USE prestashop;
-UPDATE ps_configuration SET value = NULL WHERE ps_configuration.id_configuration = 9;
-UPDATE ps_shop_url SET domain = 'localhost:8080', domain_ssl = 'localhost:8443' WHERE ps_shop_url.id_shop_url = 1;
-UPDATE ps_configuration SET value = 'localhost:8080' WHERE ps_configuration.id_configuration = 229;
-UPDATE ps_configuration SET value = 'localhost:8443' WHERE ps_configuration.id_configuration = 230;
