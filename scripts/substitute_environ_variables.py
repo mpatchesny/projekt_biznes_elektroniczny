f9cd7db2d8cad711e4be056ebeea6c21562cb410
MP <paczesnymaciej@gmail.com>
2023-01-05 13:23:06 +0100

    change LOCAL_HTTP, LOCAL_HTTPS names to HOST_HTTP, HOST_HTTPS

--- scripts/substitute_environ_variables.py
+++ scripts/substitute_environ_variables.py
@@ -9,18 +9,18 @@ with open(env_path, "r") as f:
             db_name = line.split("=")[1]
         if line.startswith("DOMAIN_NAME"):
             domain_name = line.split("=")[1]
-        if line.startswith("LOCAL_HTTP="):
-            local_http = line.split("=")[1]
-        if line.startswith("LOCAL_HTTPS="):
-            local_https = line.split("=")[1]
+        if line.startswith("HOST_HTTP="):
+            host_http = line.split("=")[1]
+        if line.startswith("HOST_HTTPS="):
+            host_https = line.split("=")[1]
 
 with open("../mysql/presta_db_config_template.sql", "r") as f:
     input = f.read()
 
 input = input.replace(r"${DB_NAME}", db_name)
 input = input.replace(r"${DOMAIN_NAME}", domain_name)
-input = input.replace(r"${LOCAL_HTTP}", local_http)
-input = input.replace(r"${LOCAL_HTTPS}", local_https)
+input = input.replace(r"${HOST_HTTP}", host_http)
+input = input.replace(r"${HOST_HTTPS}", host_https)
 print(input)
 
 with open("../mysql/presta_db_config.sql", "w") as f:
