e7e627903fc8f538fd40bb4c92f8c437a96a838c
MP <paczesnymaciej@gmail.com>
2023-01-05 13:29:20 +0100

    update

--- mysql/presta_db_config_template.sql
+++ mysql/presta_db_config_template.sql
@@ -1,5 +1,6 @@
 USE ${DB_NAME};
 UPDATE ps_configuration SET value = NULL WHERE ps_configuration.id_configuration = 9;
-UPDATE ps_shop_url SET domain = '${DOMAIN_NAME}:${HOST_HTTP}', domain_ssl = '${DOMAIN_NAME}:${HOST_HTTPS}' WHERE ps_shop_url.id_shop_url = 1;
+UPDATE ps_shop_url SET domain = '${DOMAIN_NAME}:${HOST_HTTP}' WHERE ps_shop_url.id_shop_url = 1;
 UPDATE ps_configuration SET value = '${DOMAIN_NAME}:${HOST_HTTP}' WHERE ps_configuration.id_configuration = 229;
+UPDATE ps_shop_url SET domain_ssl = '${DOMAIN_NAME}:${HOST_HTTPS}' WHERE ps_shop_url.id_shop_url = 1;
 UPDATE ps_configuration SET value = '${DOMAIN_NAME}:${HOST_HTTPS}' WHERE ps_configuration.id_configuration = 230;
