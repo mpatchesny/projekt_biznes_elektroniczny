664f51a55ad1bffbd3042a063e9f9f23c2603eb5
MP <paczesnymaciej@gmail.com>
2023-01-08 18:58:12 +0100

    update

--- scripts/init_database.sh
+++ scripts/init_database.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-SERVICE_NAME=$1
-
-docker cp ../mysql/database_dump.sh  $SERVICE_NAME:/home/database_dump.sh
-docker cp ../mysql/init_database.sql $SERVICE_NAME:/home/init_database.sql
-docker cp ../mysql/init_database.sql $SERVICE_NAME:/home/presta_db_config.sql
-
-docker container exec $SERVICE_NAME chmod +x /home/init_database.sh
-docker container exec $SERVICE_NAME chmod +x /home/database_dump.sh
-docker container exec $SERVICE_NAME chmod +x /home/presta_db_config.sh
-docker container exec $SERVICE_NAME mysql -h $DB_SERVER -uroot -pstudent /home/init_database.sh
-docker container exec $SERVICE_NAME mysql -h $DB_SERVER -uroot -pstudent $DB_NAME /home/database_dump.sh
-docker container exec $SERVICE_NAME mysql -h $DB_SERVER -uroot -pstudent $DB_NAME /home/presta_db_config.sh
-docker container exec $SERVICE_NAME rm /home/init_database.sh
-docker container exec $SERVICE_NAME rm /home/database_dump.sh
+mysql -h $DB_SERVER -uroot -pstudent /home/init_database.sh
+mysql -h $DB_SERVER -uroot -pstudent $DB_NAME /home/database_dump.sh
+mysql -h $DB_SERVER -uroot -pstudent $DB_NAME /home/presta_db_config.sh
